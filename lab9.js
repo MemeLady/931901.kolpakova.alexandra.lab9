6594d9891214ecf1904e9df98e40a6431ef436b5
MemeLady <alex.kolpakova2001@gmail.com>
2021-12-13 18:52:18 +0700

    lab 9

--- lab9.js
+++ lab9.js
@@ -1,4 +1,27 @@
 function insert(num){
-	document.form.textView.value = 
-	document.form.textView.value + num;
-}
+	if ((num == '+') || (num == '-') || (num == '*') || (num == '/')){
+		document.form.textView.value = document.form.textView.value + " " + num + " ";
+		document.form.textView.style.color='gray';
+	}
+	else{
+		document.form.textView.value = document.form.textView.value + num;
+		document.form.textView.style.color='black';
+	}	
+};	
+function clean(){
+	document.form.textView.value = "";
+	document.form.textView.style.color='black';
+};
+function back(){
+	var exp = document.form.textView.value;
+	document.form.textView.value = exp.substring(0,exp.length-1);
+	document.form.textView.style.color='black';
+
+};
+function equal(){
+	var exp = document.form.textView.value;
+	if(exp){
+		document.form.textView.value = eval(exp);
+	}
+	document.form.textView.style.color='black';
+};
