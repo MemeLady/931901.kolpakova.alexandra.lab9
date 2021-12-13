b49b1ed39986d9f04b771c064ea1ade1c6f35448
MemeLady <alex.kolpakova2001@gmail.com>
2021-12-13 20:39:46 +0700

    fix

--- lab9.js
+++ lab9.js
@@ -1,27 +1,29 @@
 function insert(num){
+	num.style='black';
 	if ((num == '+') || (num == '-') || (num == '*') || (num == '/')){
 		document.form.textView.value = document.form.textView.value + " " + num + " ";
 		document.form.textView.style.color='gray';
 	}
 	else{
-		document.form.textView.value = document.form.textView.value + num;
-		document.form.textView.style.color='black';
+		document.form.textView.value = document.form.textView.value + num;	
 	}	
 };	
 function clean(){
 	document.form.textView.value = "";
-	document.form.textView.style.color='black';
 };
 function back(){
 	var exp = document.form.textView.value;
-	document.form.textView.value = exp.substring(0,exp.length-1);
-	document.form.textView.style.color='black';
-
+	if (exp.slice(-1) == ' '){
+		document.form.textView.value = exp.substring(0,exp.length-3);
+	}
+	else{
+		document.form.textView.value = exp.substring(0,exp.length-1);
+	}	
 };
 function equal(){
 	var exp = document.form.textView.value;
 	if(exp){
 		document.form.textView.value = eval(exp);
+		document.form.textView.style.color='black';
 	}
-	document.form.textView.style.color='black';
 };
